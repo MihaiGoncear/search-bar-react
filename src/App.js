2ff9267a1c2f34fd71de455f60f04bd8944babd6
Mihai Goncear <m.goncearm@gmail.com>
2021-05-25 01:41:59 +0300

    added history working functions and fixed the bugs that were required

--- src/App.js
+++ src/App.js
@@ -1,74 +1,76 @@
 import React, { Component } from 'react';
-import { SearchBar, SubmitButton } from './Components/StyledComponents'
-import { SearchResult } from './Components/SearchResult'
-import { NameArray } from './Components/NameArray'
+import { SearchBar, SubmitButton } from './Components/StyledComponents';
+import { SearchResult } from './Components/SearchResult';
+import { NameArray } from './Components/NameArray';
+import _ from 'lodash';
+import { withRouter } from 'react-router-dom';
 import './App.css'
+import history from './history';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      input: '',
+      input: history.location.pathname.substring(1),
       array: NameArray,
-      choosedName: '',
+      searchString: '',
     };
     this.handleChangeInput = this.handleChangeInput.bind(this);
     this.chooseName = this.chooseName.bind(this);
-    this.deleteValueOfInput = this.deleteValueOfInput.bind(this);
   }
+
+  search = (str) => {
+    this.setState({
+      searchString: str
+    })
+    history.push({
+      pathname: '/' + str,
+    })
+  }
+
+  debounceSearch = _.debounce(e => this.search(e), 1000)
 
   handleChangeInput = (e) => {
     this.setState({
       input: e.target.value,
-      choosedName: ''
     })
-
+    this.debounceSearch(e.target.value)
   }
 
   chooseName = (e) => {
     this.setState({
-      choosedName: e.target.innerText,
-      input: '',
+      input: e.target.innerText,
     })
-  }
-
-  deleteValueOfInput = (e) => {
-    this.setState({
-      choosedName: '',
-      input: ''
-    })
+    this.search(e.target.innerText)
   }
 
   render() {
-
-    console.log(this.state.input)
-
     return (
       <div className="container">
-
         <form className="container__form">
           <SearchBar
             onChange={this.handleChangeInput}
             type='text'
-            onFocus={this.deleteValueOfInput}
-            value={this.state.choosedName === '' ? this.state.input : this.state.choosedName}
+            value={this.state.input}
             placeholder='Search...'
+
           />
-          <SubmitButton><span>&#128270;</span></SubmitButton>
+          <SubmitButton>
+            {this.state.choosedName === '' ? <span>&#128270;</span> : <p>&#128270;</p>}
+          </SubmitButton>
         </form>
 
         <SearchResult
           className="container__elements"
           StateStatus={this.state.status}
           ChooseName={this.chooseName}
-          InputValue={this.state.input}
+          InputValue={this.state.searchString}
           NameArray={this.state.array}
         />
-
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
