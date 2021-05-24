4aee86d9db353b58ce9c6fec60d35c6378e8b3ed
Mihai Goncear <m.goncearm@gmail.com>
2021-05-25 01:44:58 +0300

    --

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
     })
   }
 
-  debounceSearch = _.debounce(e => this.search(e), 1000)
+  debounceSearch = _.debounce(e => this.search(e), 400)
 
   handleChangeInput = (e) => {
     this.setState({
